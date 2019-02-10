55f5523a3562d418a7f793983b1316b48a37c5dd
Nadika Bandara <nsbandara32@gmail.com>
2019-02-10 16:30:22 -0600

    Updates

---
+++ precache-manifest.7525e02be968160d0181e2d3a7d890f8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c757564e0437a2b7c2ed",
+    "url": "/currentTemperature/static/js/runtime~main.c757564e.js"
+  },
+  {
+    "revision": "52b9397d5cd79f612340",
+    "url": "/currentTemperature/static/js/main.52b9397d.chunk.js"
+  },
+  {
+    "revision": "65a888d45dad63b7f23e",
+    "url": "/currentTemperature/static/js/1.65a888d4.chunk.js"
+  },
+  {
+    "revision": "52b9397d5cd79f612340",
+    "url": "/currentTemperature/static/css/main.b44b7d3c.chunk.css"
+  },
+  {
+    "revision": "8ef1cde50c797d178460639ff5cb2b1a",
+    "url": "/currentTemperature/index.html"
+  }
+];
